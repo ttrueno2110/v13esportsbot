6a4317783e5ac4a5a374254ac2f52a7bfc06e790
Broomlord <jack.17.durbin@gmail.com>
2021-09-07 21:27:29 -0400

    the actual bot that works and everything

--- commands/test.js
+++ commands/test.js
@@ -1,10 +1,24 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
+const { MessageActionRow, MessageButton, MessageEmbed } = require('discord.js');
 
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('test')
-		.setDescription('test command for various functions'),
+		.setDescription('debugging command'),
 	async execute(interaction) {
-		await interaction.reply('testing testing 1 2 1 2');
+		const row = new MessageActionRow()
+		.addComponents(
+			new MessageButton()
+				.setCustomId('bazinga')
+				.setLabel('bazinga')
+				.setStyle('PRIMARY'),
+		);
+		const embed = new MessageEmbed()
+			.setColor('#0099ff')
+			.setTitle('I Wonder What This Video is?')
+			.setURL('https://www.youtube.com/watch?v=VVjNMH5OxGc')
+			.setDescription('I guess theres only one way to find out!');
+
+			await interaction.reply({ content: 'Pong!', ephemeral: true, embeds: [embed], components: [row] });
 	},
 };
--- index.js
+++ index.js
@@ -3,18 +3,18 @@ const { Client, Collection, Intents } = require('discord.js');
 const { token } = require('./config.json');
 
 const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
-client.commands = new Collection();
 
+client.commands = new Collection();
 const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
 const eventFiles = fs.readdirSync('./events').filter(file => file.endsWith('.js'));
 
+
 for (const file of commandFiles) {
 	const command = require(`./commands/${file}`);
-	// set a new item in the Collection
-	// with the key as the command name and the value as the exported module
+	// Set a new item in the Collection
+	// With the key as the command name and the value as the exported module
 	client.commands.set(command.data.name, command);
 }
-
 for (const file of eventFiles) {
 	const event = require(`./events/${file}`);
 	if (event.once) {
@@ -24,17 +24,13 @@ for (const file of eventFiles) {
 	}
 }
 
-// this doesnt work right now. currently, user isnt identified and 'client' isnt being passed through. copy-paste code doesnt even work smh
 client.on('interactionCreate', async interaction => {
 	if (!interaction.isCommand()) return;
 
-	//tries to find a file with the command's name in the commands folder
 	const command = client.commands.get(interaction.commandName);
 
-	//Checks to see if there is a valid command
 	if (!command) return;
 
-	//Attempts to run the command, resulting in an error if one pops up.
 	try {
 		await command.execute(interaction);
 	} catch (error) {
@@ -43,4 +39,5 @@ client.on('interactionCreate', async interaction => {
 	}
 });
 
+// Login to Discord with your client's token
 client.login(token);
